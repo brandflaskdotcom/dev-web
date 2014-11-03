af01e4e293dd562bdafce554ff6738f035fc060d
Jaehong Park <psalty@gmail.com>
2014-11-03 06:11:26 +0000

    Added selector

--- www/public/js/button.init.js
+++ www/public/js/button.init.js
@@ -2,26 +2,46 @@ function init_buttons(category)
 {
   $( "#nav_top50" ).click(function() {
     var apiUrl = '/api/v1/get/top/50';
+    $("a.nav-selected").removeClass("nav-selected");
+    if(!$(this).hasClass("nav-selected")) {
+      $(this).addClass("nav-selected");
+    }
     update_chart(apiUrl, "top", 1);
   });
 
   $( "#nav_luxury" ).click(function() {
     var apiUrl = '/api/v1/get/' + category[2] + '/50';
+    $("a.nav-selected").removeClass("nav-selected");
+    if(!$(this).hasClass("nav-selected")) {
+      $(this).addClass("nav-selected");
+    }
     update_chart(apiUrl, category[2], 1);
   });
 
   $( "#nav_casual" ).click(function() {
     var apiUrl = '/api/v1/get/' + category[1] + '/50';
+    $("a.nav-selected").removeClass("nav-selected");
+    if(!$(this).hasClass("nav-selected")) {
+      $(this).addClass("nav-selected");
+    }
     update_chart(apiUrl, category[1], 1);
   });
 
   $( "#nav_boutique" ).click(function() {
     var apiUrl = '/api/v1/get/' + category[0] + '/50';
+    $("a.nav-selected").removeClass("nav-selected");
+    if(!$(this).hasClass("nav-selected")) {
+      $(this).addClass("nav-selected");
+    }
     update_chart(apiUrl, category[0], 1);
   });
 
   $( "#nav_mbs" ).click(function() {
     var apiUrl = '/api/v1/get/' + category[3] + '/50';
+    $("a.nav-selected").removeClass("nav-selected");
+    if(!$(this).hasClass("nav-selected")) {
+      $(this).addClass("nav-selected");
+    }
     update_chart(apiUrl, category[3], 1);
   });
 
